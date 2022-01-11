b4ef6ba3fe61acc08b69d00ce05a8247d5c5e04c
rladbrua0207 <rladbrua0207@gmail.com>
2022-01-12 01:59:12 +0900

    6-4 leastRecentlyUsed

---
+++ inflearnAlgorithm/src/SortingAndSearching_6/leastRecentlyUsed_R.java
@@ -0,0 +1,38 @@
+package SortingAndSearching_6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class leastRecentlyUsed_R {
+    public ArrayList solution(int N, int S, int[] task) {
+        ArrayList<Integer> cache = new ArrayList();
+        cache.add(task[0]);
+        for(int index = 1; index < N;){
+            if(cache.contains(task[index])){
+                for (int i = 0; i < cache.size(); i++) {
+                    if (task[index] == cache.get(i)) cache.remove(i);
+                }
+            }else{
+                if (cache.size() == S) cache.remove(S - 1);
+            }
+            cache.add(0, task[index++]);
+        }
+        return cache;
+    }
+
+    public static void main(String[] args) throws IOException {
+        leastRecentlyUsed_R T = new leastRecentlyUsed_R();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[] task = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) task[i] = Integer.parseInt(st.nextToken());
+        ArrayList answer = T.solution(N, S, task);
+        for(int i = 0; i < answer.size(); i++) System.out.print(answer.get(i) + " ");
+    }
+}
