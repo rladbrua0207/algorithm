f26a318d75a5861e3ede73ca5168c35d078b314c
rladbrua0207 <rladbrua0207@gmail.com>
2022-02-12 02:04:02 +0900

    백준 Q2606 - 바이러스

---
+++ baekjoon/src/winter2021/Q2606.java
@@ -0,0 +1,52 @@
+package winter2021;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Q2606 {
+    static ArrayList<ArrayList<Integer>> arr;
+    static int[] check;
+    static int answer = 0;
+
+    static void DFS(int L) {
+        if(check[1] == 0){
+            return;
+        }else{
+            for (int a : arr.get(L)) {
+                if (check[a] == 0) {
+                    check[a] = 1;
+                    answer++;
+                    DFS(a);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int P = Integer.parseInt(br.readLine());
+
+        arr = new ArrayList<>();
+        check = new int[N + 1];
+        check[1] = 1;
+
+        for (int i = 0; i <= N; i++) arr.add(new ArrayList<>());
+
+        for (int i = 0; i < P; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr.get(a).add(b);
+            arr.get(b).add(a);
+        }
+        DFS(1);
+        System.out.println(answer);
+    }
+}
