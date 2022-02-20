9681fcc91aa134e539e7c2f094e41cd051408304
rladbrua0207 <rladbrua0207@gmail.com>
2022-02-21 01:25:36 +0900

    bin파일 변동

---
+++ inflearnAlgorithm/src/no_8_DFS_BFS활용/no_5_동전교환.java
@@ -0,0 +1,50 @@
+package no_8_DFS_BFS활용;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class no_5_동전교환 {
+    static int[] check;
+    static ArrayList<Integer> kinds;
+    static int M, answer = Integer.MAX_VALUE;
+
+    public static void DFS(int L, int sum) {
+        if (sum > M) return;
+        if(L>= answer) return;
+        if (sum == M) {
+            answer = Math.min(answer, L);
+            return;
+        } else {
+            for (int i = 0; i < kinds.size(); i++) {
+                if(check[sum+ kinds.get(i)] == 0){
+                    DFS(L + 1, sum + kinds.get(i));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        kinds = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            kinds.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(kinds);
+        Collections.reverse(kinds);
+        check = new int[N * 100];
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+
+        DFS(0, 0);
+        System.out.println(answer);
+    }
+}
---
+++ inflearnAlgorithm/src/no_8_DFS_BFS활용/no_7_조합수.java
@@ -0,0 +1,25 @@
+package no_8_DFS_BFS활용;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class no_7_조합수 {
+    static int[][] dy = new int[35][35];
+    public static int DFS(int n, int r){
+        if(dy[n][r] > 0) return dy[n][r];
+        if(n == r || r == 0) return 1;
+        else return dy[n][r] = DFS(n-1,r-1) + DFS(n-1, r);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        System.out.println(DFS(n,r));
+    }
+}
