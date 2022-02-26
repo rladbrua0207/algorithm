1032598d834b9fff0c4dba9cc3c3552a6c2b0de4
rladbrua0207 <rladbrua0207@gmail.com>
2022-02-26 14:29:59 +0900

    인프런 10-2 돌다리 건너기

--- inflearnAlgorithm/src/no_10_dynamic_programming/no_2_돌다리건너기.java
+++ inflearnAlgorithm/src/no_10_dynamic_programming/no_2_돌다리건너기.java
@@ -4,21 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class no_1_계단오르기 {
+public class no_2_돌다리건너기 {
     static int[] dy;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        dy = new int[N+1];
+        dy = new int[N+2];
 
         dy[1] = 1;
         dy[2] = 2;
 
-        for(int i = 3; i <= N; i++){
+        for(int i = 3; i <= N+1; i++){
             dy[i] = dy[i-1] + dy[i-2];
         }
-        System.out.println(dy[N]);
+        System.out.println(dy[N+1]);
     }
 }
