ff06c47353d2ed18b6e0c331c68837ac0dfa2133
rladbrua0207 <rladbrua0207@gmail.com>
2022-02-27 13:46:57 +0900

    백준 Q11053 가장 긴 증가하는 부분 수열

---
+++ baekjoon/src/winter2021/Q11053.java
@@ -0,0 +1,31 @@
+package winter2021;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int answer = 1;
+        int[] dy = new int[N + 1];
+        dy[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dy[i] = 1;
+            for (int j = 1; j <= i-1; j++) {
+                if (arr[i] > arr[j]) dy[i] = Math.max(dy[j] +1, dy[i]);
+            }
+            answer = Math.max(answer, dy[i]);
+        }
+        System.out.println(answer);
+    }
+}
