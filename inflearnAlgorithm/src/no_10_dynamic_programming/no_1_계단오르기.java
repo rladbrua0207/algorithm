6c23c56c101112a2c61cdc6d9f7ca74f66cf2dda
rladbrua0207 <rladbrua0207@gmail.com>
2022-02-27 13:40:20 +0900

    인프런 10-3 최대 부분 증가수열

--- inflearnAlgorithm/src/no_10_dynamic_programming/no_1_계단오르기.java
+++ inflearnAlgorithm/src/no_10_dynamic_programming/no_1_계단오르기.java
@@ -8,14 +8,10 @@ public class no_1_계단오르기 {
     static int[] dy;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-
         dy = new int[N+1];
-
         dy[1] = 1;
         dy[2] = 2;
-
         for(int i = 3; i <= N; i++){
             dy[i] = dy[i-1] + dy[i-2];
         }
