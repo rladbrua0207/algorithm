8101c5db59be78880e757e7fe00202cf16db95fe
rladbrua0207 <rladbrua0207@gmail.com>
2022-02-27 15:56:30 +0900

    백준 Q11054 가장 긴 바이토닉 부분 수열 리펙토링

--- baekjoon/src/winter2021/Q11054.java
+++ baekjoon/src/winter2021/Q11054.java
@@ -3,6 +3,7 @@ package winter2021;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q11054 {
@@ -16,7 +17,7 @@ public class Q11054 {
         int[] arr = new int[N + 1];
         for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        int answer = 1;
+        /*int answer = 1;
         int[] dy = new int[N + 1];
         dy[1] = 1;
         for (int k = 2; k <= N - 1; k++) {
@@ -32,7 +33,34 @@ public class Q11054 {
                 }
                 answer = Math.max(answer, dy[i]);
             }
+        }*/
+
+
+        int[] dy1 = new int[N + 1];
+        int[] dy2 = new int[N + 1];
+
+        Arrays.fill(dy1, 1);
+        Arrays.fill(dy2, 1);
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                if (arr[i] > arr[j]) dy1[i] = Math.max(dy1[j] + 1, dy1[i]);
+            }
         }
+
+        for (int i = N-1; i >= 1; i--) {
+            for (int j = i + 1; j <= N; j++) {
+                if (arr[i] > arr[j]) {
+                    dy2[i] = Math.max(dy2[i], dy2[j] + 1);
+                }
+            }
+        }
+
+        int answer = 0;
+        for(int i = 1; i <= N; i++){
+            answer = Math.max(answer, dy1[i] + dy2[i] -1);
+        }
+
         System.out.println(answer);
     }
 }
